04e8bbc38988c7dfe0c808833cd704ebccf996df
Miguel Angel Califa Urquiza <miguelangelcu@ufps.edu.co>
2017-11-16 20:39:59 -0500

    se envia el codigo para controlar 4 relevos

---
+++ Gaymer.ino
@@ -0,0 +1,125 @@
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+MDNSResponder mdns;
+
+// Replace with your network credentials
+const char* ssid = "construpinos";
+const char* password = "C5j23h42";
+int Reles[]= {4,5,12,13,14,16};//array to store pin nos
+bool estadoApagado = HIGH;
+bool estadoEncendido = LOW;
+ESP8266WebServer server(80);
+
+String webPage = "";
+
+//int gpio0_pin = 15;
+//int gpio2_pin = 5;
+
+void setup(void){
+  webPage += "<style>.caja{color:white;font-style: bold;margin:20px;text-align:center;}.pie{color:white;font-style: bold;margin:20px;text-align: left;font-size:20px;text-align:center;margin-top:20px;}.nombres{color:white;font-style: bold;margin:20px;text-align: left;font-size:15px;}.botones{margin:10px;margin-left:0.1%;}</style><div style='background-color:green; width: 100%;height: 100%'><h1 style='color:white;font-style:bold;text-align:center'>Control Wi-Fi para iluminacion - Ferreteria Construrama los pinos</h1><div class='caja'><p>Rele #1 <a class='botones' href='socket1On'><button>ON</button></a>&nbsp;<a class='botones' href='socket1Off'><button>OFF</button></a></p'></div><div class='caja'><p>Rele #2<a class='botones' href='socket2On'><button>ON</button></a>&nbsp;<a class='botones' href='socket2Off'><button>OFF</button></a></p'></div><div class='caja'><p>Rele #3 <a class='botones' href='socket3On'><button>ON</button></a>&nbsp;<a class='botones' href='socket3Off'><button>OFF</button></a></p'></div><div class='caja'><p>Rele #4<a class='botones' href='socket4On'><button>ON</button></a>&nbsp;<a class='botones' href='socket4Off'><button>OFF</button></a></p'></div><div class='caja'><p>Rele #5<a class='botones' href='socket5On'><button>ON</button></a>&nbsp;<a class='botones' href='socket5Off'><button>OFF</button></a></p'></div><div class='caja'><p>Rele #6<a class='botones' href='socket6On'><button>ON</button></a>&nbsp;<a class='botones' href='socket6Off'><button>OFF</button></a></p'></div><footer class='pie'><p>Agradecimientos</p><a class='nombres' href='mailto:gaymersteevenmv@ufps.edu.co'>Gaymer Mendoza</a><a class='nombres' href='mailto:Jhonatanricardomg@ufps.edu.co'>Jhonatan Montoya</a><a class='nombres' href='mailto:yormanalexanderm@ufps.edu.co'>Yorman Mendoza</a></footer></div>";
+  // preparing GPIOs
+    for(int i = 0; i<6 ; i++)
+    {
+      pinMode(Reles[i], OUTPUT);
+      digitalWrite(Reles[i],estadoApagado);
+    }
+  
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  if (mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+  
+  server.on("/", [](){
+    server.send(200, "text/html", webPage);
+  });
+
+  server.on("/socket1On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[0], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket1Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[0], estadoApagado);
+    delay(500); 
+  });
+   server.on("/socket2On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[1], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket2Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[1], estadoApagado);
+    delay(500); 
+  });
+   server.on("/socket3On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[2], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket3Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[2], estadoApagado);
+    delay(500); 
+  });
+   server.on("/socket4On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[3], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket4Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[3], estadoApagado);
+    delay(500); 
+  });
+  server.on("/socket5On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[4], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket5Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[4], estadoApagado);
+    delay(500); 
+  });
+
+  server.on("/socket6On", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[5], estadoEncendido);
+    delay(500);
+  });
+  server.on("/socket6Off", [](){
+    server.send(200, "text/html", webPage);
+    digitalWrite(Reles[5], estadoApagado);
+    delay(500); 
+  });
+
+
+  server.begin();
+  Serial.println("Servidor iniciado");
+}
+ 
+void loop(void){
+  server.handleClient();
+} 
+
---
+++ readme.md
@@ -0,0 +1,37 @@
+Control domotico de 4 relevos con ESP-07 interfaz WEB
+
+
+Este es un sistema para controlar el encendido y apagado de 4 relevos y se hizo empleando el sistema
+embebido ESP - 07 realizado por Gaymer Mendoza y Jhonathan  Ricardo .
+Para esto se conectan 6 relevos en los pines  ( 4,5,12,13,14,16) correspondientes a la placa ESP-07
+y se programo con la interfaz FDTI  A 3.3 VDC, se empleo dos fuentes de alimentacion , una para los
+relevos y una para el ESP-07 para evitar problemas de falla de corriente.
+
+==========
+
+requisitos:
+
+--------------------
+
+Hardware:
+
++ ESP-07.
++ Modulo de 6 relevos x 2 unidades.
++ 2 fuentes DC-DC para protoboard 3.3v - 5v.
++ 2 adaptadores DC 12V.
+
+Hardware para programar:
+
++ Tarjeta programadora serial FDTI 3.3V 
+
+
+Software:
+
++ Arduino ide (www.arduino.cc).
++ Arduino board manager for ESP module --> https://github.com/esp8266/Arduino/releases/download/2.4.0-rc1/package_esp8266com_index.json
+
+Contacto:
+
++ gaymersteevenmv@ufps.edu.co
++ Jhonatanricardomg@ufps.edu.co
++ miguelangelcu@ufps.edu.co - 3192597748
